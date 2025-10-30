3a242e1778b81ba03d3ad7ec434e3b8f13205fcb
Arafat Hossain <164580721+arafathosen28@users.noreply.github.com>
2025-10-30 13:05:38 +0600

    Delete functions/src/Bal directory

--- functions/src/Bal/index.ts
+++
@@ -1,9 +0,0 @@
-import * as admin from 'firebase-admin'
-import { createOrder } from './createOrder'
-import { bkashWebhook, nagadWebhook } from './webhooks'
-
-if (!admin.apps.length) {
-  admin.initializeApp()
-}
-
-export { createOrder, bkashWebhook, nagadWebhook }
